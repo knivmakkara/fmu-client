7d1f99aa089850253f1ec49c72fe8aaa419b37e9
kristoffer <kristoffer.wikstrm@gmail.com>
2014-09-11 18:28:23 +0200

    Work on menu system

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,19 +28,44 @@ angular
         url: "/overview",
         abstract: true,
         controller: 'OverviewCtrl',
-        templateUrl: "views/overview.html",
+        templateUrl: "views/overview/overview.html",
     })
     .state('overview.orders', {
         url: "/orders",
-        templateUrl: "views/overview.orders.html",
+        templateUrl: "views/overview/orders.html",
     })
     .state('overview.ongoing', {
         url: "/ongoing",
-        templateUrl: "views/overview.ongoing.html",
+        templateUrl: "views/overview/ongoing.html",
     })
     .state('overview.completed', {
         url: "/completed",
-        templateUrl: "views/overview.completed.html",
-    });
+        templateUrl: "views/overview/completed.html",
+    })
+    .state('eavrop', {
+        url: "/eavrop/{eavropId:[0-9]+}",
+        abstract: true,
+        templateUrl: "views/eavrop/eavrop.html",
+    })
+    .state('eavrop.order', {
+        url: "/order",
+        templateUrl: "views/eavrop/order.html",
+    })
+    .state('eavrop.allevents', {
+        url: "/all-events",
+        templateUrl: "views/eavrop/all-events.html",
+    })
+    .state('eavrop.compensation', {
+        url: "/compensation",
+        templateUrl: "views/eavrop/compensation.html",
+    })
+    .state('eavrop.investigation', {
+        url: "/investigation",
+        templateUrl: "views/eavrop/investigation.html",
+    })
+    .state('eavrop.notes', {
+        url: "/notes",
+        templateUrl: "views/eavrop/notes.html",
+    })
 });
 
